f846587fae5ad817dd5391370634e3e512573617
Juan <segnanajuan@gmail.com>
2021-08-25 00:05:14 +0000

    modif. para replit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { scheduleJob } from 'node-schedule';
+import { scheduleJob, RecurrenceRule } from 'node-schedule';
 import hacerBusqueda from './jobs/hacerBusqueda';
 
 const main = async (): Promise<void> => {
     await require('./bot');
     // await require('./server');
     // Debe iniciar a las 22:05, los martes, jueves y sábados.
-    const CRON_JOB = '5 22 * * 2,4,6';
-    scheduleJob(CRON_JOB, () => {
+    const rule = new RecurrenceRule();
+    rule.hour = 22;
+    rule.minute = 5;
+    rule.dayOfWeek = [2,4,6];
+    rule.tz = 'America/Argentina/Buenos_Aires';
+    // const CRON_JOB = '5 22 * * 2,4,6';
+    scheduleJob(rule, () => {
         console.log('Empezando búsqueda...');
         hacerBusqueda();
     });
